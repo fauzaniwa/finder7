3def38de01f7c8da5d78b74ffe2d142d32045f35
EyeesL <miftahulfadli223@gmail.com>
2025-08-28 23:35:04 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirect to homepage.php</title>
+</head>
+<body>
+    <script>
+        window.location.href = 'homepage.php';
+    </script>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+</body>
+</html>
